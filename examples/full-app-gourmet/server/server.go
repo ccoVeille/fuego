c8a8fa943355d4fdcb304e92e659c32502c6376f
EwenQuim <ewen.quimerch@gmail.com>
2024-08-28 19:13:12 +0200

    Variadic route options

--- examples/full-app-gourmet/server/server.go
+++ examples/full-app-gourmet/server/server.go
@@ -9,7 +9,16 @@ import (
 	"github.com/go-fuego/fuego/examples/full-app-gourmet/static"
 	"github.com/go-fuego/fuego/examples/full-app-gourmet/templates"
 	"github.com/go-fuego/fuego/examples/full-app-gourmet/views"
+	"github.com/go-fuego/fuego/option"
 )
+
+func cache(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=600")
+
+		h.ServeHTTP(w, r)
+	})
+}
 
 type Ressources struct {
 	Views views.Ressource
@@ -40,16 +49,12 @@ func (rs Ressources) Setup(
 	fuego.Use(app, chiMiddleware.Compress(5, "text/html", "text/css", "application/json"))
 
 	fuego.Register(app, fuego.Route[any, any]{
-		Path:     "/static/",
-		Method:   http.MethodGet,
-		FullName: "static handler",
-	}, http.StripPrefix("/static", static.Handler()), func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Cache-Control", "public, max-age=600")
-
-			h.ServeHTTP(w, r)
-		})
-	})
+		BaseRoute: fuego.BaseRoute{
+			Path:     "/static/",
+			Method:   http.MethodGet,
+			FullName: "static handler",
+		},
+	}, http.StripPrefix("/static", static.Handler()), option.Middleware(cache))
 
 	// Register views (controllers that return HTML pages)
 	rs.Views.Routes(fuego.Group(app, "/"))
